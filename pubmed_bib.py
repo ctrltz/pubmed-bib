8bfeb850d89b86e72295da6c735f54dad6b9410e
Nikolai Kapralov <4dvlup@gmail.com>
2025-06-19 10:22:31 +0200

    Escape capital letters in the title, add DOI, change ref_id

--- pubmed_bib.py
+++ pubmed_bib.py
@@ -3,8 +3,9 @@ import requests
 import json
 import re
 
+
 # Get a reference from PubMed database
-def getReference(id):
+def get_reference(id):
     url_format = 'https://api.ncbi.nlm.nih.gov/lit/ctxp/v1/pubmed/'
     # Parameters for query
     queryParams = {
@@ -15,13 +16,16 @@ def getReference(id):
     response = requests.get(url_format, params = queryParams).json()
     return id, response
 
+
 # Format the reference to BibTex format
-def formatReference(reference, use_short):
+def format_reference(reference, use_short):
     id, reference = reference
     title = reference['title'] if 'title' in reference.keys() else ''
     # convert <sub> and <sup>> to latex
     title = re.sub("<sub>(.+)</sub>", "$_{\\1}$", title)
     title = re.sub("<sup>(.+)</sup>", "$^{\\1}$", title)
+    # wrap capital letters in curly brackets
+    title = re.sub("([A-Z]+)", "{\\1}", title)
 
     authors = reference['author'] if 'author' in reference.keys() else ''
     authorList = []
@@ -39,15 +43,16 @@ def formatReference(reference, use_short):
     journal_short = reference.get('container-title-short') or ''
     volume = reference.get('volume') or ''
     page = reference.get('page') or ''
+    doi = reference.get('DOI', '')
     
     if 'issued' in reference.keys():
         year = reference['issued']['date-parts'][0][0]
     elif 'epub-date' in reference.keys():
         year = reference['epub-date']['date-parts'][0][0]    
 
-    ref_id = authors[0]["family"].lower() \
+    ref_id = authors[0]["family"].title() \
             if "family" in authors[0].keys() else authors[0]
-    ref_id += str(year) + title.split(' ')[0].lower()
+    ref_id += str(year)
 
     output = f'''@article{{{ ref_id },
     title={{{title}}},
@@ -57,53 +62,55 @@ def formatReference(reference, use_short):
     volume={{{volume}}},
     pages={{{page}}},
     year={{{year}}},
-    PMID={{{id}}}
+    doi={{{doi}}}
 }}
 '''
     return output
 
-def showReference(id, use_short):
+
+def show_reference(id, use_short):
     '''
     Get the BibTex styled reference from a given PMID from the PubMed database 
     using PubMed's API
     '''
     # Get the reference from PubMed
-    reference = getReference(id)
+    reference = get_reference(id)
     # I the reference is not found
     if 'status' in reference[1].keys() and reference[1]['status'] == 'error' :
         output = 'Reference not found'
     else:
-        output = formatReference(reference, use_short)
+        output = format_reference(reference, use_short)
 
     click.echo(output)
     return
 
-def saveReference(id, path, use_short):
+
+def save_reference(id, path, use_short):
     '''
     Append a reference to an bib file
     '''
-    reference = getReference(id)
+    reference = get_reference(id)
     if 'status' in reference[1].keys() and reference[1]['status'] == 'error' :
         click.echo("Reference not found")
         return
     with open(path, "a") as f:
-        f.write(formatReference(reference, use_short))
+        f.write(format_reference(reference, use_short))
     return
         
 
-def convertReferences(input_file, output_file):
+def convert_references(input_file, output_file):
     # Read all PMIDs in
     id = []
     successNumber, failNumber = 0, 0
     with open(input_file) as ih:
         with open(output_file, 'w') as oh:
             for line in ih:
-                ref = getReference(line.rstrip())
+                ref = get_reference(line.rstrip())
                 if 'status' in ref.keys() and ref['status'] == 'error' :
                     print(f'PMID {line.rstrip()} NOT FOUND')
                     failNumber += 1
                 else: 
-                    ref = formatReference(ref)
+                    ref = format_reference(ref)
                     successNumber += 1
                     oh.write(ref+'\n')
     print(f'{successNumber} reference{"s" if successNumber > 1 else ""} ' +\
@@ -126,11 +133,11 @@ def pubMed2BibTex(id, input_file, output_file, short_journal):
     '''
     if id:
         if output_file:
-            saveReference(id, output_file, short_journal)
+            save_reference(id, output_file, short_journal)
         else:
-            showReference(id, short_journal)
+            show_reference(id, short_journal)
     elif input_file and output_file:
-        convertReferences(input_file, output_file, short_journal)
+        convert_references(input_file, output_file, short_journal)
     else:
         return
 
